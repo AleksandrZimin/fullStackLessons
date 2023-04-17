8bf4cd7cc7ee6f0100366c50027dc181f997a21d
Aleksandr Zimin <uzao-rec@ya.ru>
2023-04-17 04:53:37 +0300

    feature: update all and add js

--- app/js/animated.js
+++ app/js/animated.js
@@ -6,36 +6,43 @@ const yaHideBlock = document.querySelector(".ya-learning");
 const itHideBlock = document.querySelector(".it-learning");
 const gbHideBlock = document.querySelector(".gb-learning");
 
-const hideBlock = function (nameBlock, nameTitle) {
+const arrowRotate = document.querySelector(".arrow");
+const yaArrowRotate = document.querySelector(".ya-arrow");
+const itArrowRotate = document.querySelector(".it-arrow");
+const gbArrowRotate = document.querySelector(".gb-arrow");
+
+const hideBlock = function (nameBlock, nameTitle, nameArrow) {
    nameBlock.classList.add('close-open');
    nameTitle.setAttribute('title', 'Нажмите, чтобы развернуть');
+   nameArrow.classList.add('arror-rotate');
 }
 
-const openBlock = function (nameBlock, nameTitle) {
+const openBlock = function (nameBlock, nameTitle, nameArrow) {
    nameBlock.classList.remove('close-open');
    nameTitle.setAttribute('title', 'Нажмите, чтобы свернуть');
+   nameArrow.classList.remove('arror-rotate');
 }
 
 yablockButton.addEventListener('click', (event) => {
    if (yaHideBlock.classList.contains('close-open')) {
-      openBlock(yaHideBlock, yablockButton);
+      openBlock(yaHideBlock, yablockButton, yaArrowRotate);
    } else {
-      hideBlock(yaHideBlock, yablockButton);
+      hideBlock(yaHideBlock, yablockButton, yaArrowRotate);
    }
 });
 
 itblockButton.addEventListener('click', (event) => {
    if (itHideBlock.classList.contains('close-open')) {
-      openBlock(itHideBlock, itblockButton);
+      openBlock(itHideBlock, itblockButton, itArrowRotate);
    } else {
-      hideBlock(itHideBlock, itblockButton);
+      hideBlock(itHideBlock, itblockButton, itArrowRotate);
    }
 });
 
 gbblockButton.addEventListener('click', (event) => {
    if (gbHideBlock.classList.contains('close-open')) {
-      openBlock(gbHideBlock, gbblockButton);
+      openBlock(gbHideBlock, gbblockButton, gbArrowRotate);
    } else {
-      hideBlock(gbHideBlock, gbblockButton);
+      hideBlock(gbHideBlock, gbblockButton, gbArrowRotate);
    }
 });
